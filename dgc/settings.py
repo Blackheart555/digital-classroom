88e45a655a3850e0171ba8b6f17ca72affe45224
Blackheart555 <49880343+Blackheart555@users.noreply.github.com>
2024-10-14 15:18:22 +0600

    Update settings.py

--- dgc/settings.py
+++ dgc/settings.py
@@ -86,16 +86,15 @@ WSGI_APPLICATION = 'dgc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3"
-        # 'HOST': os.getenv('DB_HOST'),
-        # 'NAME': os.getenv('DB_NAME'),
-        # 'USER': os.getenv('DB_USER'),
-        # 'PASSWORD': os.getenv('DB_PASS'),
-        # 'PORT': os.getenv('DB_PORT'),
-        # 'OPTIONS': {
-        #     'client_encoding': 'utf8mb4',
-        # }
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+        'OPTIONS': {
+            'client_encoding': 'utf8mb4',
+        }
     }
 }
 
