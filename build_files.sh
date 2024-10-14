da035a046218f66fa673b6d4b142c1710a45b07c
Blackheart555 <49880343+Blackheart555@users.noreply.github.com>
2024-10-14 12:12:18 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 echo " BUILD START"
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
-python3.12 manage.py makemigrations
-python3.12 manage.py migrate
 echo " BUILD END"
